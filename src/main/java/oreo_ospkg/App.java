eb4f858e50c2e78d012ae57547784971a6ec3429
KETHAVATH RAKESH NAIK <114587685+KRakeshnaik@users.noreply.github.com>
2024-07-13 22:44:20 +0530

    Add files via upload

---
+++ src/main/java/oreo_ospkg/App.java
@@ -0,0 +1,13 @@
+package oreo_ospkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
